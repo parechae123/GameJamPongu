3db91e7c3678731b263991a26f1ef0c3d3cb9c7a
junhee4613 <junhee000123@naver.com>
2023-06-27 13:11:02 +0900

    푸쉬

--- PongGu/Assets/02.Scripts/MiddleWall.cs
+++ PongGu/Assets/02.Scripts/MiddleWall.cs
@@ -17,28 +17,12 @@ public class MiddleWall : MonoBehaviour
             sequence = DOTween.Sequence();
             sequence.Append(wall.transform.DOScale(new Vector2(1.3f, 1.3f), 1));
             sequence.Append(wall.transform.DOScale(new Vector2(1, 1), 0.5f));
-
-
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*wallScale += Time.deltaTime;
-        if(wall.transform.localScale.x <= 1)
-        {
-            wall.transform.localScale = new Vector2(1, 1) * wallScale;
-
-        }
-        else if(wall.transform.localScale.x <= 1.5f)
-        {
-            wall.transform.localScale = new Vector2(1.5f, 1.5f) * wallScale * 2;
-        }
-        else
-        {
-            wall.transform.localScale = new Vector2(1, 1) * wallScale * 2;
-        }*/
         
     }
 }
