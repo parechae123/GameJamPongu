9f5321307d032518f3f8d7acb7153ebb2ecd2f90
junhee4613 <junhee000123@naver.com>
2023-06-27 15:02:28 +0900

    푸쉬

--- PongGu/Assets/02.Scripts/MiddleWall.cs
+++ PongGu/Assets/02.Scripts/MiddleWall.cs
@@ -5,24 +5,35 @@ using DG.Tweening;
 public class MiddleWall : MonoBehaviour
 {
     private bool wallActive = true;
-    public GameObject wall;
+    public GameObject[] wall = new GameObject[2];
     Sequence sequence;
     public float wallScale;
+    GameManager GameManager => GameManager.GMinstance();
     // Start is called before the first frame update
     void Start()
     {
-        if (wallActive)
-        {
-            wall.SetActive(true);
-            sequence = DOTween.Sequence();
-            sequence.Append(wall.transform.DOScale(new Vector2(1.3f, 1.3f), 1));
-            sequence.Append(wall.transform.DOScale(new Vector2(1, 1), 0.5f));
-        }
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (true)
+        {
+            StartCoroutine(WallAddCollider()); 
+        }
+
+    }
+    public IEnumerator WallAddCollider()
+    {
+        for (int i = 0; i < wall.Length; i++)
+        {
+            wall[i].SetActive(true);
+            sequence = DOTween.Sequence();
+            sequence.Append(wall[i].transform.DOScale(new Vector2(1.3f, 1.3f), 0.7f));
+            sequence.Append(wall[i].transform.DOScale(new Vector2(1, 1), 0.3f));
+        }
+        yield return new WaitForSeconds(1);
         
     }
 }
